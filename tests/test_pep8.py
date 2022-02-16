892960c834794526bcf3c150f791ba94caa474bf
josev814 <josev814@gmail.com>
2022-02-16 09:38:48 -0500

    disabling pep8 addon
    using dist focal and using a matrix to ensure that python 3.10 is tested
    
    adding the CI=true env variable
    
    Adjusted the test_pep8 to only import the StyleGuide rather than all of pep8 for it's test

--- tests/test_pep8.py
+++ tests/test_pep8.py
@@ -1,12 +1,12 @@
 '''test pysftp module, setup.py and tests - uses py.test'''
-import pep8
+from pep8 import StyleGuide
 
 
 def test_pep8():
     '''pep8 check the source'''
     # list the specific files or directories to check, directories are recursed
     paths = ['pysftp', 'setup.py', 'tests']
-    p8c = pep8.StyleGuide()
+    p8c = StyleGuide(quiet=True)
     report = p8c.check_files(paths=paths)
     report.print_statistics()
     assert report.get_count() == 0
